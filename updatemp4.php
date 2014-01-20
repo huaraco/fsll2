a1ddf106ec0041607a0b776d88b28a5682df2656
huaraco <huaraco@outlook.com>
2014-01-20 10:45:57 +0800

    test

---
+++ updatemp4.php
@@ -0,0 +1,18 @@
+<?php
+	// require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+	// $x = $wpdb->get_results('select * from wp_posts');
+	// foreach( $x as $item ) {
+	//     $content =  $item->post_content;
+	//     if (strpos($content,'.wmv') !== false){
+	//     	$content = str_replace('.wmv', '.mp4', $content);
+	//     }
+	//     $wpdb->update( 
+	// 		'wp_posts', 
+	// 		array( 
+	// 			'post_content' => $content,	// string
+	// 		), 
+	// 		array( 'ID' => $item->ID )
+	// 	);
+	// }
+
+?>
